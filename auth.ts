c5cf919c00f0d1e13c365210fa53f90627f4b233
Tarcisio Couto <tarcisiocouto@MacBook-Air-de-Tarcisio.local>
2024-12-04 14:07:32 -0300

    Descrição das mudanças realizadas

--- auth.ts
+++ auth.ts
@@ -1,9 +1,18 @@
-import { PrismaAdapter } from '@/lib/auth/prisma-adpter'
-import { prisma } from '@/lib/prisma'
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
+import { PrismaAdapter } from '@/lib/auth/prisma-adpter'
+import { prisma } from '@/lib/prisma'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma), // Conecta o Prisma Adapter ao banco
-  providers: [Google],
+  adapter: PrismaAdapter(prisma), // Conecta ao Prisma Adapter
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          scope: 'openid email profile',
+          prompt: 'consent',
+        },
+      },
+    }),
+  ],
 })
